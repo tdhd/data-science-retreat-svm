1a7c1a5bf7c2702810505884d34683c3a2d00865
philipp <tdhd@users.noreply.github.com>
2018-07-14 03:27:12 -0700

    update amazon script

--- data_project/sentiment/amazon_reviews.py
+++ data_project/sentiment/amazon_reviews.py
@@ -3,9 +3,9 @@ import pandas as pd
 # http://jmcauley.ucsd.edu/data/amazon/
 # http://snap.stanford.edu/data/amazon/productGraph/categoryFiles/reviews_Cell_Phones_and_Accessories_5.json.gz
 
-df = pd.read_json('[%s]' % ','.join(open('../reviews_Amazon_Instant_Video_5.json').readlines()))
+df = pd.read_json('[%s]' % ','.join(open('../reviews_Baby_5.json').readlines()))
 
-print df.describe()
+print(df.describe())
 
 # reviewerID - ID of the reviewer, e.g. A2SUAM1J3GNN3B
 # asin - ID of the product, e.g. 0000013714
@@ -16,20 +16,20 @@ print df.describe()
 # summary - summary of the review
 # unixReviewTime - time of the review (unix time)
 # reviewTime - time of the review (raw)
-print df.columns
+print(df.columns)
 
 # print df.overall.value_counts()
 # print df.reviewText.str.len()
-print df.reviewerID.value_counts()
-print df[df.reviewerID == 'A36SL1P1YOIXL5']
+print(df.reviewerID.value_counts())
+print(df[df.reviewerID == 'A36SL1P1YOIXL5'])
 
 df['sl'] = df.summary.str.len()
 df['rl'] = df.reviewText.str.len()
 
-print df.corr('pearson')
+print(df.corr('pearson'))
 
-print df.asin.value_counts()
+print(df.asin.value_counts())
 
-print df[df.asin == 'B00I3MPDP4'].overall.mean()
+print(df[df.asin == 'B00I3MPDP4'].overall.mean())
 
-print df.shape
+print(df.shape)
